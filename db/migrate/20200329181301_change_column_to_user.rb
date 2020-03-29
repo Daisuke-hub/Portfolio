3560e09ec4189b6bfc11cf2e7453bc3bf76dca9a
Daisuke-hub <general.daisuke@gmail.com>
2020-03-30 05:07:23 +0900

    [fix] Userテーブルのintroductionカラム修正

--- db/migrate/20200329181301_change_column_to_user.rb
+++ db/migrate/20200329181301_change_column_to_user.rb
@@ -1,5 +1,9 @@
 class ChangeColumnToUser < ActiveRecord::Migration[5.2]
-  def change
-    remove_index :users, :introduction
+  def up
+    change_column :users, :introduction, :string, index: true
+  end
+
+  def down
+    change_column :users, :introduction, :text, index: true
   end
 end
