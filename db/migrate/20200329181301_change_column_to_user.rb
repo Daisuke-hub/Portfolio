3a2847fd7416e6c663d6089a4196a8545307f5f4
Daisuke-hub <general.daisuke@gmail.com>
2020-03-30 20:45:26 +0900

    [fix] Userテーブルのintroductionカラム修正 => limit_255

--- db/migrate/20200329181301_change_column_to_user.rb
+++ db/migrate/20200329181301_change_column_to_user.rb
@@ -1,6 +1,6 @@
 class ChangeColumnToUser < ActiveRecord::Migration[5.2]
   def up
-    change_column :users, :introduction, :string, index: true
+    change_column :users, :introduction, :text, index: true, limit: 255
   end
 
   def down
