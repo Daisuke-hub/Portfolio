cc3c61a5b9c99b1c6756cd7f54995b869126eaf6
Daisuke-hub <general.daisuke@gmail.com>
2020-04-09 01:15:06 +0900

    [add] サイト説明の追加

--- app/assets/javascripts/abouts.js
+++ app/assets/javascripts/abouts.js
@@ -1,2 +1,20 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+$(function() {
+    $('#about_move').on('click',function(){
+        $('body, html').animate({
+        scrollTop:600
+        }, 600);
+        return false;
+        // $(window).scrollTop(600);
+    });
+});
+
+// $(function() {
+//     $(".about_head").on("click",function(){
+//         $(".about_head, .about_body").animate({
+//             "opacity": 0
+//         });
+//     });
+// });
--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,6 @@ Rails.application.routes.draw do
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, only: [:index, :show, :edit, :update]
-  get "about", to: "abouts#about"
   get "top", to: "abouts#top"
 
   mount ActionCable.server => "/cable"
