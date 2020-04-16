8d4524b546dab831edb4bc31459e8f7d9e45ceeb
Daisuke-hub <general.daisuke@gmail.com>
2020-04-17 02:13:46 +0900

    [fix] インデントの修正

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,38 +17,38 @@
 //= require_tree .
 
 $(function(){
+  history.pushState(null, null, null);
+  $(window).on("popstate", function (event) {
     history.pushState(null, null, null);
-    $(window).on("popstate", function (event) {
-      history.pushState(null, null, null);
-    });
+  });
 });
 
 // ページ上部に推移
 $(function() {
-    $('#page_top, #about_botton').on('click',function(){
-        $('body, html').animate({
-        scrollTop:0
-        }, 600);
-        return false;
-    });
+  $('#page_top, #about_botton').on('click',function(){
+    $('body, html').animate({
+      scrollTop:0
+    }, 600);
+    return false;
+  });
 });
 
 // プッシュ通知の許可申請
 $(function(){
-    $('#push').on('click',function(){
-        if (!('Notification' in window)) {
-            alert('お使いのブラウザは通知機能非対応です');
-            }else{
-            alert("ブラウザの通知設定を有効にすることで、チャットメッセージを受信した時に、プッシュ通知を受け取ることができます。(ブラウザによっては通知を受け取れない場合があります。)");
-            Notification.requestPermission()
-                .then((permission) => {
-                if (permission == 'granted') {
-                    var notification = new Notification('通知が許可されました');
-                } else if (permission == 'denied') {
-                    var notification = new Notification('通知を拒否しました');
-                } else if (permission == 'default') {
-                }
-            });
-        };
-    });
+  $('#push').on('click',function(){
+    if (!('Notification' in window)) {
+        alert('お使いのブラウザは通知機能非対応です');
+      }else{
+        alert("ブラウザの通知設定を有効にすることで、チャットメッセージを受信した時に、プッシュ通知を受け取ることができます。(ブラウザによっては通知を受け取れない場合があります。)");
+        Notification.requestPermission()
+        .then((permission) => {
+        if (permission == 'granted') {
+          var notification = new Notification('通知が許可されました');
+        } else if (permission == 'denied') {
+          var notification = new Notification('通知を拒否しました');
+        } else if (permission == 'default') {
+        }
+      });
+    };
+  });
 });
--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -2,22 +2,22 @@
 // All this logic will automatically be available in application.js.
 
 $(function(){
-    $("#user_user_image").change(function(){
-        $(".attachment").remove();
-        var selectedFile = this.files[0];
-        var fileReader = new FileReader();
-        fileReader.onloadend = function(event){
-            var loadedImageUri = event.target.result
-            $("#result").html('<img src="' + loadedImageUri + '"id=preview >' );
-        }
-        fileReader.readAsDataURL(selectedFile);
-    });
+  $("#user_user_image").change(function(){
+    $(".attachment").remove();
+    var selectedFile = this.files[0];
+    var fileReader = new FileReader();
+    fileReader.onloadend = function(event){
+      var loadedImageUri = event.target.result
+      $("#result").html('<img src="' + loadedImageUri + '"id=preview >' );
+    }
+    fileReader.readAsDataURL(selectedFile);
+  });
 });
 
 $(function(){
-    $("#page_count_move").animate({
-        "paddingTop": "10px"
-    }).animate({
-        "paddingTop": "0px"
-    });
+  $("#page_count_move").animate({
+    "paddingTop": "10px"
+  }).animate({
+    "paddingTop": "0px"
+  });
 });
