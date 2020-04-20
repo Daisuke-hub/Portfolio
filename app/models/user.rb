9b6de550a4f738755c0845a6764b048342f20c14
Daisuke-hub <general.daisuke@gmail.com>
2020-04-20 17:22:33 +0900

    [fix] ユーザ情報編集機能の修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,7 +16,7 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :introduction, length: {maximum: 300}
   validates :name, length: {maximum: 10}
-  validates :age, numericality: {only_integer: true}, inclusion: { in: 1..200 }, allow_blank: true
+  validates :age, numericality: {only_integer: true}, inclusion: { in: 18..100 }, allow_blank: true
   acts_as_paranoid
   attachment :user_image
 
